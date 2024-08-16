77bffc5b4a14e1aceda06d01761209fe9f160e6a
Daniel Kuntz <dkuntz0@gmail.com>
2024-08-15 17:42:57 -0700

    Remove ffmpeg dependency

--- Package.swift
+++ Package.swift
@@ -11,14 +11,13 @@ let package = Package(
             targets: ["YoutubeDL"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kewlbear/FFmpeg-iOS-Lame", from: "0.0.6-b20230730-000000"),
         .package(url: "https://github.com/pvieito/PythonKit.git", from: "0.3.1"),
         .package(url: "https://github.com/kewlbear/Python-iOS.git", from: "0.1.1-b"),
     ],
     targets: [
         .target(
             name: "YoutubeDL",
-            dependencies: ["Python-iOS", "PythonKit", "FFmpeg-iOS-Lame"]),
+            dependencies: ["Python-iOS", "PythonKit"]),
         .testTarget(
             name: "YoutubeDL_iOSTests",
             dependencies: ["YoutubeDL"]),
